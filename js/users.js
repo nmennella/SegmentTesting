e5a3876c201a0aaecaa32c800dc1f2a5e1659509
nmennella <mennella.nick@gmail.com>
2022-03-10 10:41:13 -0600

    Create users.js

---
+++ js/users.js
@@ -0,0 +1,47 @@
+let users = {};
+
+users.luigi = {
+  userId: "80085",
+  traits: {
+    name: "Luigi Mario",
+    email: "luigi@brothersplumbing.it",
+    address: {
+      street: "1 Up St",
+      city: "Toad Town",
+      state: "Dinosour Land",
+      postalCode: "94025",
+      country: "Mushroom Kingdom"
+    }
+  }
+};
+
+users.mario = {
+  userId: "58008",
+  traits: {
+    name: "Mario Mario",
+    email: "mario@brothersplumbing.it",
+    address: {
+      street: "1 Up St",
+      city: "Toad Town",
+      state: "Dinosour Land",
+      postalCode: "94025",
+      country: "Mushroom Kingdom"
+    }
+  }
+};
+
+users.homer = {
+  userId: 'smrt0001',
+  traits: {
+    name: "Homer Simpson",
+    email: "homer@snpp.com",
+    address: {
+      street: "742 Evergreen Terrace",
+      city: "Springfield",
+      state: "Springfield's State",
+      postalCode: "94112",
+      country: "USA"
+    }
+  }
+
+}
