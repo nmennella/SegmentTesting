a52ed06a7cb9f9fcd2c0fa577288bfdbac3b287e
nmennella <mennella.nick@gmail.com>
2022-03-10 10:39:13 -0600

    Create funnels.js

---
+++ js/funnels.js
@@ -0,0 +1,44 @@
+let funnels = {};
+
+funnels.allEvents = [
+  "productsSearched",
+  "productListViewed",
+  "productListFiltered",
+  "promotionViewed",
+  "promotionClicked ",
+  "productClicked",
+  "productViewed",
+  "productAdded",
+  "productRemoved",
+  "cartViewed",
+  "checkoutStarted",
+  "checkoutStepViewed",
+  "checkoutStepCompleted",
+  "paymentInfoEntered",
+  "orderCompleted",
+  "orderUpdated",
+  "orderRefunded",
+  "orderCancelled",
+  "couponEntered",
+  "couponApplied",
+  "couponDenied",
+  "couponRemoved",
+  "productAddedToWishlist",
+  "productRemovedFromWishlist",
+  "wishlistProductAddedToCart",
+  "productShared",
+  "cartShared",
+  "productReviewed"
+];
+
+funnels.funnel = [
+  "productsSearched",
+  "productViewed",
+  "productAdded",
+  "cartViewed",
+  "checkoutStarted",
+  "checkoutStepViewed",
+  "checkoutStepCompleted",
+  "paymentInfoEntered",
+  "orderCompleted"
+];
